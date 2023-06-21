459fd1b1c7cf8417e12b4799375444e53627c9c6
virtutae <virtutae@yahoo.it>
2023-06-21 07:40:13 +0100

    commit with the file imported and JEST test fail

---
+++ src/S-find-shortest-string.test.js
@@ -0,0 +1,13 @@
+// import function from local file
+const findShortestString = require("./S-find-shortest-string");
+
+test("findShortestString finds the shortest string in an array", () => {
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["camera", "mouses"])).toBe("camera");
+  expect(findShortestString(["two", "six"])).toBe("two");
+});
